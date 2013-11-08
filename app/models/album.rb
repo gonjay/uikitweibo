50d6a3edc046208378d2431c21a707e0293c5639
gonjay <assyer@gmail.com>
2013-11-08 11:46:31 +0800

    fix some problem

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,8 +1,10 @@
+#!/bin/env ruby  
+# encoding: utf-8
 class Album < ActiveRecord::Base
   attr_accessible :imageUrl, :useType, :user_id, :useFlag
 
   def useFlagType
-    type = ["普通照片","", "形象照片"]
+    type = ["普通照","", "形象照"]
     type[self.useFlag] if self.useFlag && self.useFlag < 3
   end
 end
